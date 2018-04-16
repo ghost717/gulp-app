d4292184c3b6339405cd06a3c5894666c7df57a4
ghost717 <dawid.zawadzki7717@gmail.com>
2018-04-16 11:59:33 +0200

    add custom excerpt function

--- functions.php
+++ functions.php
@@ -31,6 +31,32 @@ function my_custom_formatting($content){
     endif;
 }
 
+
+/**
+ * Filter the excerpt "read more" string.
+ *
+ * @param string $more "Read more" excerpt string.
+ * @return string (Maybe) modified "read more" excerpt string.
+ */
+function wpdocs_excerpt_more( $more ) {
+    return '...';
+}
+add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
+
+
+/**
+ * Filter the except length to 20 words.
+ *
+ * @param int $length Excerpt length.
+ * @return int (Maybe) modified excerpt length.
+*/
+
+function wpdocs_custom_excerpt_length( $length ) {
+    return 20;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+
+
 /**
  * enqueue scripts and styles 
  * GOOGLE MAP APIS
