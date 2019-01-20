c6ee2fca1f6198880bde3b73da00793de34c0eed
ghost717 <dawid.zawadzki7717@gmail.com>
2019-01-20 23:25:28 +0100

    simple contact view

--- page-contact.php
+++ page-contact.php
@@ -8,11 +8,20 @@ get_header(); ?>
       <section class="main__page__contact">
 
         <div class="wrap content --relative">
-          <?php while (have_posts()) : the_post(); ?>
+            <?php while (have_posts()) : the_post(); ?>
 
+                <?php while(have_rows('mapy')): the_row(); ?>
+                
+                    <?php $location = get_sub_field('mapa'); ?>
+                    
+                    <div class="acf_map">
+                        <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>" data-address="<?php echo $location['address']; ?>"></div>
+                        <p class="address"><?php echo $location['address']; ?></p>
+                    </div>
+                    
+                <?php endwhile; ?>
 
-
-          <?php endwhile; ?>
+            <?php endwhile; ?>
         </div>
 
       </section><!-- main__page__contact -->
