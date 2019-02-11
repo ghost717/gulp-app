df9add40a5419b27612eecca27dda82e6a173527
ghost717 <dawid.zawadzki7717@gmail.com>
2019-02-11 23:56:48 +0100

    securities added

--- functions.php
+++ functions.php
@@ -367,6 +367,27 @@ register_taxonomy(
 // remove the wp version
 remove_action('wp_head', 'wp_generator');
 
+function my_secure_generator( $generator, $type ) {
+	return '';
+}
+add_filter( 'the_generator', 'my_secure_generator', 10, 2 );
+
+function my_remove_src_version( $src ) {
+	global $wp_version;
+
+	$version_str = '?ver='.$wp_version;
+	$offset = strlen( $src ) - strlen( $version_str );
+
+	if ( $offset >= 0 && strpos($src, $version_str, $offset) !== FALSE )
+		return substr( $src, 0, $offset );
+
+	return $src;
+}
+add_filter( 'script_loader_src', 'my_remove_src_version' );
+add_filter( 'style_loader_src', 'my_remove_src_version' );
+
+add_filter('xmlrpc_enabled', '__return_false');
+
 // svg support
 function cc_mime_types($mimes)
 {
